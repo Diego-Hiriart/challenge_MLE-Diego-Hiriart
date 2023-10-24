0ecd52425145d9a19ec23e1796110b33f35b6628
Diego-Hiriart <hiriart.leon.d@gmail.com>
2023-10-24 16:34:08 -0500

    feat: performance improvement
    improve performance by removing generation of unused features

--- challenge/model.py
+++ challenge/model.py
@@ -16,48 +16,7 @@ class DelayModel:
         self._model = None # Model should be saved in this attribute.
         self._dataScale = 0# For scaling data
     
-    # Methods for feature generation
-    def get_period_day(self, date):
-        date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
-        morning_min = datetime.strptime("05:00", '%H:%M').time()
-        morning_max = datetime.strptime("11:59", '%H:%M').time()
-        afternoon_min = datetime.strptime("12:00", '%H:%M').time()
-        afternoon_max = datetime.strptime("18:59", '%H:%M').time()
-        evening_min = datetime.strptime("19:00", '%H:%M').time()
-        evening_max = datetime.strptime("23:59", '%H:%M').time()
-        night_min = datetime.strptime("00:00", '%H:%M').time()
-        night_max = datetime.strptime("4:59", '%H:%M').time()
-        
-        if(date_time > morning_min and date_time < morning_max):
-            return 'mañana'
-        elif(date_time > afternoon_min and date_time < afternoon_max):
-            return 'tarde'
-        elif(
-            (date_time > evening_min and date_time < evening_max) or
-            (date_time > night_min and date_time < night_max)
-        ):
-            return 'noche'
-    
-    def is_high_season(self, fecha):
-        fecha_año = int(fecha.split('-')[0])
-        fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
-        range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
-        range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
-        range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
-        range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
-        range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
-        range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
-        range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
-        range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
-        
-        if ((fecha >= range1_min and fecha <= range1_max) or 
-            (fecha >= range2_min and fecha <= range2_max) or 
-            (fecha >= range3_min and fecha <= range3_max) or
-            (fecha >= range4_min and fecha <= range4_max)):
-            return 1
-        else:
-            return 0
-    
+    # Method for feature generation    
     def get_min_diff(self, data):
         fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
         fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
@@ -83,10 +42,6 @@ class DelayModel:
             pd.DataFrame: features.
         """
         # Generate features
-        # Get "Period of Day" deature
-        data['period_day'] = data['Fecha-I'].apply(self.get_period_day)
-        # Get "High Season" feature
-        data['high_season'] = data['Fecha-I'].apply(self.is_high_season)
         #Get "Difference in Minutes" feature
         data['min_diff'] = data.apply(self.get_min_diff, axis = 1)
         # Get "Delay" feature
